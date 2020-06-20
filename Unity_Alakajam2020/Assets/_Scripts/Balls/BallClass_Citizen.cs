b40b1481b03c0b4be2f081f478afa25137627aa1
Mads Vesterager Riddersholm <>
2020-06-20 23:04:21 +0200

    changed the simple ball to ball_citizen for better overview of ball types

--- Unity_Alakajam2020/Assets/_Scripts/Balls/BallClass_Citizen.cs
+++ Unity_Alakajam2020/Assets/_Scripts/Balls/BallClass_Citizen.cs
@@ -2,17 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallClass_Citizen : MonoBehaviour
+public class BallClass_Citizen : BaseBall
 {
-    // Start is called before the first frame update
-    void Start()
+    public InfectedSettings classSetting;
+
+    public override void OnEnable()
     {
-        
+        m_cachedTriggerRadius = classSetting.triggerRadius;
+        m_cachedLifeTime = classSetting.lifeTime;
+        m_cachedVirusLevel = classSetting.virusLevel;
+        m_cachedSpeed = classSetting.speed;
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void OnDeath()
     {
-        
+        base.OnDeath();
+
+        StartCoroutine(DeathAnimation());
+    }
+
+    public override void SetInfected()
+    {
+
+        if (infected)
+        {
+            return;
+        }
+
+        infected = infectedSetting;
+
+        base.SetInfected();
+
+        //triggerArea.transform.localScale = transform.localScale * m_cachedTriggerRadius; 
     }
 }
