b230e6b42f015b3eba4ed22e223c4cbc60e273d5
Jlindskov <jeppelind@icloud.com>
2020-06-20 12:39:25 +0200

    BaseBall

---
+++ Unity_Alakajam2020/Assets/_Scripts/BaseCollision.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCollision : MonoBehaviour
+{
+    
+    public 
+    
+    
+    public virtual void OnCollisionEnter(Collision other)
+    {
+        
+        
+        
+        throw new NotImplementedException();
+    }
+}
