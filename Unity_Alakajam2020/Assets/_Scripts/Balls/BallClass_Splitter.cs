be57e226937b8d6e6895b9a570c8c04d03cd0573
Jlindskov <jeppelind@icloud.com>
2020-06-20 23:51:45 +0200

    Splitter

---
+++ Unity_Alakajam2020/Assets/_Scripts/Balls/BallClass_Splitter.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallClass_Splitter : BaseBall
+{
+    public InfectedSettings classSetting;
+    public GameObject prefab;
+    public int spawnAmount = 2;
+
+    public override void OnEnable()
+    {
+        m_cachedTriggerRadius = classSetting.triggerRadius;
+        m_cachedLifeTime = classSetting.lifeTime;
+        m_cachedVirusLevel = classSetting.virusLevel;
+        m_cachedSpeed = classSetting.speed;
+    }
+
+    
+    protected override void OnDeath()
+    {
+        base.OnDeath();
+
+        StartCoroutine(DeathAnimation());
+
+        for (int i = 0; i < spawnAmount; i++)
+        {
+            Instantiate(prefab, transform.position, Quaternion.identity); 
+        }
+    }
+
+    public override void SetInfected()
+    {
+
+        if (infected)
+        {
+            return;
+        }
+
+        infected = infectedSetting;
+
+        base.SetInfected();
+
+        //triggerArea.transform.localScale = transform.localScale * m_cachedTriggerRadius; 
+    }
+}
