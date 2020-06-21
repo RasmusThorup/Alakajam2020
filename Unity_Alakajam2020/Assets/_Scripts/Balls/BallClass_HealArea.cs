035a4d4515fe2cffec2a221d10c879a8b59d3b3e
Mads Vesterager Riddersholm <>
2020-06-21 16:04:22 +0200

    Deez nutz are made for walking, and that's just what the'll do

---
+++ Unity_Alakajam2020/Assets/_Scripts/Balls/BallClass_HealArea.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallClass_HealArea : BaseBall
+{
+    public InfectedSettings classSetting;
+
+    public override void OnEnable()
+    {
+        m_cachedTriggerRadius = classSetting.triggerRadius;
+        m_cachedLifeTime = classSetting.lifeTime;
+        m_cachedVirusLevel = classSetting.virusLevel;
+        m_cachedSpeed = classSetting.speed;
+
+        base.OnEnable();
+        isMedic = true;
+        StartCoroutine(ScaleUp());
+    }
+
+    protected override void OnTriggerEnter(Collider other)
+    {
+        BaseBall otherBall = other.GetComponentInParent<BaseBall>();
+
+        if (otherBall.infected)
+        {
+            float healChance = m_cachedVirusLevel / (m_cachedVirusLevel + otherBall.m_cachedVirusLevel);
+            
+            if (Random.value < healChance)
+            {
+                otherBall.infected = false;
+                otherBall.SetHealed();
+            }
+            else
+            {
+                OnDeath();
+            }
+        }
+    }
+
+
+    public override void SetInfected()
+    {
+        if (infected)
+        {
+            return;
+        }
+        //infected = overrideSettings;
+
+        StartCoroutine(ScaleToNormal());
+        base.SetInfected();
+    }
+
+    protected override void OnDeath()
+    {
+        base.OnDeath();
+        StartCoroutine(DeathAnimation());
+    }
+}
---
+++ Unity_Alakajam2020/Assets/_Scripts/Balls/BallClass_SuperInfected.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallClass_SuperInfected : BaseBall
+{
+    public InfectedSettings classSetting;
+
+    public override void OnEnable()
+    {
+
+        m_cachedTriggerRadius = citizenSetting.triggerRadius;
+        m_cachedLifeTime = citizenSetting.lifeTime;
+        m_cachedVirusLevel = citizenSetting.virusLevel;
+        m_cachedSpeed = citizenSetting.speed;
+
+        //infected = true;
+        base.OnEnable();
+        //StartCoroutine(ScaleUp());
+    }
+
+    public override void SetInfected()
+    {
+        if (infected)
+        {
+            return;
+        }
+
+        m_cachedTriggerRadius = classSetting.triggerRadius;
+        m_cachedLifeTime = classSetting.lifeTime;
+        m_cachedVirusLevel = classSetting.virusLevel;
+        m_cachedSpeed = classSetting.speed;
+
+        base.SetInfected();
+        m_cachedSpeed = 0;
+
+    }
+
+    protected override void OnDeath()
+    {
+        base.OnDeath();
+        StartCoroutine(DeathAnimation());
+    }
+}
