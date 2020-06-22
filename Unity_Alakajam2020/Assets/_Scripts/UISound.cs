7f3c3dfce1317500f7bd310adcd132e6db8d20ad
Mads Vesterager Riddersholm <>
2020-06-22 20:35:30 +0200

    implemented audio

---
+++ Unity_Alakajam2020/Assets/_Scripts/UISound.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISound : MonoBehaviour
+{
+
+    public void PlaySound()
+    {
+        AkSoundEngine.PostEvent("ui_button_activate", this.gameObject);
+    }
+}
